ffc930de76aa10595e712c9a02b654bd81b602ec
Doori Kim <doori.alice.kim@gmail.com>
2019-08-29 21:01:22 +0900

    사이드바  수정중....

--- src/Components/Home/Navbar.js
+++ src/Components/Home/Navbar.js
@@ -1,25 +1,50 @@
 import React from "react";
-// import Menu from "../../Pages/Menu/Menu";
+import Menu from "../../Pages/Menu/Menu";
 import "./Navbar.scss";
-
 class Navbar extends React.Component {
   constructor() {
     super();
     this.state = {};
   }
+  // openMenu = () => {
+  //   return (
+  //     <div className="menu_container">
+  //       <ul>
+  //         <li>X</li>
+  //         <li>Home</li>
+  //         <li>About</li>
+  //         <li>Recipes</li>
+  //         <li>Food News</li>
+  //       </ul>
+  //     </div>
+  //   );
+  // };
+  // closeMenu = () => {};
+  openMenu = () => {
+    return <Menu />;
+  };
   render() {
-    // nav_drawer = () => {
-    //   <Menu />;
-    // };
     return (
-      <>
+      <div className="nav_container">
+        <div className="menu_container">
+          {/* <ul>
+            <li>Home</li>
+            <li>About</li>
+            <li>Recipes</li>
+            <li>Food News</li>
+          </ul> */}
+        </div>
         <nav className="nav_bar">
           <div className="menu_wrap">
             <img
               className="menu_btn"
               src={require("../../img/menu_btn.png")}
               alt="menu_btn"
-              // onClick={this.nav_drawer}
+              onClick={this.openMenu}
+              // onClick={(this.menu_container.style.overflow = "visible")}
+              // onClick={(this.menu_container.style.left = 0 + `px`)}
+              // onClick={this.openMenu}
+              // onClick={this.openMenu}
             />
           </div>
           <div className="logo_wrap">
@@ -39,7 +64,7 @@ class Navbar extends React.Component {
             />
           </div>
         </nav>
-      </>
+      </div>
     );
   }
 }
