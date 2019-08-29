93258165dcdf3dfaefb312638f086933168a22b2
Doori Kim <doori.alice.kim@gmail.com>
2019-08-29 22:45:14 +0900

    reset css setting

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from "react";
 import ReactDOM from "react-dom";
-import "./index.css";
+import "../src/styles/index.scss";
+import "../src/styles/reset.scss";
 import Routes from "./Routes";
 
 ReactDOM.render(<Routes />, document.getElementById("root"));
