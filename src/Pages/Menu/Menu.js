78a64073109e428d4164db9df7c53f757950c906
Doori Kim <doori.alice.kim@gmail.com>
2019-08-29 22:28:38 +0900

    side bar modified

--- src/Pages/Menu/Menu.js
+++ src/Pages/Menu/Menu.js
@@ -1,16 +1,17 @@
 import React from "react";
+import "./Menu.scss";
 
 class Menu extends React.Component {
   render() {
     return (
-      <>
+      <div className="menu_container">
         <ul>
           <li>Home</li>
           <li>About</li>
           <li>Recipes</li>
           <li>Food News</li>
         </ul>
-      </>
+      </div>
     );
   }
 }
