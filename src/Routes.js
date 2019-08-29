9f880306c5eed486e85eeb48146ecdb290040926
hkhch <macbook@Macbooks-MacBook-Pro.local>
2019-08-29 19:57:39 +0900

    Signup 완료(SERVER API확인완료)

--- src/Routes.js
+++ src/Routes.js
@@ -3,15 +3,18 @@ import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
 import Home from "./Pages/Home/Home";
 import RecipePage from "./Pages/Recipe/RecipePage";
 import Login from "./Pages/Login/Login";
+import Signup from "./Pages/Signup/Signup";
 
 class Routes extends React.Component {
   render() {
     return (
       <Router>
         <Switch>
+          <Route exact path="/" component={Home} />
           <Route exact path="/home" component={Home} />
           <Route exact path="/recipe_page" component={RecipePage} />
           <Route exact path="/login" component={Login} />
+          <Route exact path="/signup" component={Signup} />
         </Switch>
       </Router>
     );
