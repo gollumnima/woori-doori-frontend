72eba1ef363b23cf26650bca9a0d7496bb1bafec
Doori Kim <doori.alice.kim@gmail.com>
2019-08-29 21:05:42 +0900

    add about routes

--- src/Routes.js
+++ src/Routes.js
@@ -1,5 +1,6 @@
 import React from "react";
 import { BrowserRouter as Router, Route, Switch } from "react-router-dom";
+import About from "./Pages/About/About";
 import Home from "./Pages/Home/Home";
 
 class Routes extends React.Component {
@@ -8,6 +9,7 @@ class Routes extends React.Component {
       <Router>
         <Switch>
           <Route exact path="/" component={Home} />
+          <Route exact path="/about" component={About} />
           <Route exact path="/home" component={Home} />
         </Switch>
       </Router>
