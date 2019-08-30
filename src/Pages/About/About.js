cf886256bb8b2d01825951fc14631fb7dae5895b
Doori Kim <doori.alice.kim@gmail.com>
2019-08-30 10:43:43 +0900

    about_page_1st commit

--- src/Pages/About/About.js
+++ src/Pages/About/About.js
@@ -1,13 +1,13 @@
 import React from "react";
-// import Navbar from "../../Components/Home/Navbar";
 import Profile from "./Profile";
+import { Link } from "react-router-dom";
 import "./About.scss";
+// import { MapContainer } from "./MapContainer";
 
 class About extends React.Component {
   render() {
     return (
       <>
-        {/* <Navbar /> */}
         <div className="about_container">
           <h1 className="about_title">Meet Our Team Members!</h1>
           <div className="profile_box">
@@ -17,6 +17,19 @@ class About extends React.Component {
             <Profile />
             <Profile />
           </div>
+          <div className="map_container">{/* <MapContainer /> */}</div>
+          <Link to="Home">
+            <button>Home</button>
+          </Link>
+          <footer>
+            <ul>
+              <li>주식회사 : 우리두리닷컴</li>
+              <li>대표자명 : 김예리</li>
+              <li>사업자번호: 222-22-22222</li>
+              <li>사업장 주소 : 서울시 강남구 테헤란로 427, Graceful Rain</li>
+              <li></li>
+            </ul>
+          </footer>
         </div>
       </>
     );
--- src/Pages/About/Profile.js
+++ src/Pages/About/Profile.js
@@ -6,13 +6,16 @@ class Profile extends React.Component {
     return (
       <>
         <div className="profile_container">
-          <div>
-            <img src={require("../../img/doori.jpg")} alt="doori" />
-          </div>
-          <div className="words_wrapper">
-            <h1>김두리</h1>
-            <h2>Front-End</h2>
-            <h3>메인 담당 및 어쩌구저쩌구</h3>
+          <div className="profile_box">
+            <div className="photo_wrapper">
+              <div className="triangle"></div>
+              <img src={require("../../img/doori.jpg")} alt="doori" />
+            </div>
+            <div className="words_wrapper">
+              <h1>Doori Kim</h1>
+              <span>Front-End</span>
+              <h3>메인 담당 및 어쩌구저쩌구</h3>
+            </div>
           </div>
         </div>
       </>
