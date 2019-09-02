93711e9e2cfae32e930a6c6556b22187b38d32de
jingyong9111 <53812942+jingyong9111@users.noreply.github.com>
2019-09-02 11:36:41 +0900

    Update News_page_son.js

--- src/Pages/News_page/News_page_son.js
+++ src/Pages/News_page/News_page_son.js
@@ -1,19 +1,19 @@
 import React from "react";
-import "./News_page.scss";
+import "./NewsPage.scss";
 
-class News_page_son extends React.Component {
+class NewsPageSon extends React.Component {
   render() {
     return (
       <div className="news_page_scss">
         <div className="wrap">
-          <div className="tittle">{this.props.title} </div>
-          <div className="line"></div>
+          <div className="tittle">{this.props.title}</div>
+          {/* <div className="line"></div> */}
           <div className="newsbox_wrap">
             <div className="newsbox1">
-              <div className="img">{this.props.text} </div>
+              <div className="imgtext">{this.props.text} </div>
             </div>
             <div className="newsbox2">
-              <img className="img" src={this.props.img}></img>
+              <img src={this.props.img}></img>
             </div>
           </div>
         </div>
@@ -22,4 +22,4 @@ class News_page_son extends React.Component {
   }
 }
 
-export default News_page_son;
+export default NewsPageSon;
