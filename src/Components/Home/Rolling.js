513b9531c007fb2e09c4dc088074db9c0a5e7367
Doori Kim <doori.alice.kim@gmail.com>
2019-09-03 16:01:07 +0900

    remoce underline

--- src/Components/Home/Rolling.js
+++ src/Components/Home/Rolling.js
@@ -1,3 +1,23 @@
+// import React from "react";
+// import "./Rolling.scss";
+
+// class Rolling extends React.Component {
+//   render() {
+//     return (
+//       <div className="Carousel_container">
+//         <figure>
+//           <img src={require("../../img/steak.jpg")} alt="steak" />
+//           <img src={require("../../img/yogurt.jpg")} alt="steak" />
+//           <img src={require("../../img/kebab.jpg")} alt="steak" />
+//           <img src={require("../../img/waffle.jpg")} alt="steak" />
+//         </figure>
+//       </div>
+//     );
+//   }
+// }
+
+// export default Rolling;
+
 import React from "react";
 import Carousel from "nuka-carousel";
 import "./Rolling.scss";
