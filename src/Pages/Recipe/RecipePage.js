f5a0778df8bc465e346cd891076d174e88914cf4
Doori Kim <doori.alice.kim@gmail.com>
2019-09-06 08:55:23 +0900

    conflict complete

--- src/Pages/Recipe/RecipePage.js
+++ src/Pages/Recipe/RecipePage.js
@@ -38,33 +38,6 @@ class RecipePage extends React.Component {
         recipe_cnt: 3
       })
     })
-<<<<<<< HEAD
-      .then(response => response.json())
-      .then(response => {
-        this.setState({
-          recipeDetail: response
-        });
-      });
-  }
-  render() {
-    let { recipeDetail } = this.state;
-
-    return (
-      <div className="recipe_page_total_wrap">
-        <div className="recipe_page_container">
-          <div className="food_name">
-            <h1>{recipeDetail.name}</h1>
-          </div>
-          <div className="recipe_container">
-            <div className="cooking_process">
-              <div className="ingredients_box">
-                <h1 className="title_line">Ingredients</h1>
-                <h5> 햄, 번, 토마토, 치즈, 베이컨</h5>
-              </div>
-              <div className="recipe_box">
-                <h1 className="title_line">recipe</h1>
-                <h5>{recipeDetail.method}</h5>
-=======
       .then(response => {
         // console.log("1: ", response);
         return response.json();
@@ -104,7 +77,6 @@ class RecipePage extends React.Component {
               </div>
               <div className="foodie">
                 <img src={recipeDetail.image} alt=""></img>
->>>>>>> de2e0f78d708d78a485e577a965e7b5f57b4bada
               </div>
             </div>
             <div className="foodie">
