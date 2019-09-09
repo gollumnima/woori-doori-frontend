736170ac3fd8a0cea5ba8d67bf9a25b8967afe24
Doori Kim <doori.alice.kim@gmail.com>
2019-09-09 22:22:26 +0900

    error completed

--- src/Pages/Home/Home.js
+++ src/Pages/Home/Home.js
@@ -29,7 +29,7 @@ class Home extends React.Component {
       }
     )
       .then(response => {
-        response.json();
+        return response.json();
       })
       .then(response => {
         this.setState({
@@ -46,7 +46,7 @@ class Home extends React.Component {
       }
     )
       .then(response => {
-        response.json();
+        return response.json();
       })
       .then(response => {
         this.setState({
